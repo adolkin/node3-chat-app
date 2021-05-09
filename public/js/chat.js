f7e3e6869fa73f278b3fbf1f53240089a3a6ac01
Manh Tuan <tuanbk189@gmail.com>
2021-05-09 14:49:07 +0700

    164. Timestamps for Location Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,7 +12,6 @@ const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
-  console.log(message)
   const html = Mustache.render(messageTemplate, {
     message: message.text,
     createdAt: moment(message.createdAt).format('h:mm a')
@@ -20,9 +19,10 @@ socket.on('message', (message) => {
   $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('locationMessage', (url) => {
+socket.on('locationMessage', (message) => {
   const html = Mustache.render(locationMessageTemplate, {
-    url
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
 })
